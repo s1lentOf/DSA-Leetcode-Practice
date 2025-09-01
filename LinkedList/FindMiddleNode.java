165bfc850f1774c32042923fe23898f267f97957
Ihor Ivanchenko <ihor.ivanchenko@student.griffith.ie>
2025-09-01 19:45:11 +0200

    Changed the comments

--- LinkedList/FindMiddleNode.java
+++ LinkedList/FindMiddleNode.java
@@ -1,13 +1,12 @@
-// To find the Middle node of the LinkedList, we need to use
-// the 'Floyd's tortoise and hare cycle-finding algorithm. Thi algorithm uses two pointers:
+// To find the Middle node of the LinkedList, we need to use two pointers:
 // one pointer moving twice faster than the other. That way, when 'fast' pointer will reach the end of the LL
 // the slow pointer would be exactly on the middle (if the length of the LL is odd number) or on the next node
 // after the middle (if the length of the LL is even number).
 
 class Solution {
-    public LinkedList.Node findMiddleNode() {
-        LinkedList.Node slow = head;
-        LinkedList.Node fast = head;
+    public Node findMiddleNode(Node head) {
+        Node slow = head;
+        Node fast = head;
 
         if (slow == null) return null;
 
